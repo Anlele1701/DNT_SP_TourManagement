25a66d0b111061b301e0d28065dd91850997a341
anlele1701 <lethanhduyan@gmail.com>
2023-07-25 07:12:01 +0700

    v1.3

--- TOURDL/Controllers/DatTourController.cs
+++ TOURDL/Controllers/DatTourController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TOURDL.Models;
 
 namespace TOURDL.Controllers
 {
     public class DatTourController : Controller
     {
         // GET: DatTour
+        private TourDLEntities db = new TourDLEntities();
+
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult DatTour()
+        public ActionResult DatTour(string id)
         {
-            return View();
+            var data = db.SPTOURs.Where(s => s.ID_SPTour == id);
+            return View(data);
         }
     }
 }
